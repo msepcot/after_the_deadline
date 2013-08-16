04b7d15891ac06588e60dad09d01a02d9d8857af
Michael Sepcot <msepcot@tunein.com>
2013-08-16 10:04:14 -0700

    oops. grammar not grammer, nice mistake for this library

--- lib/after_the_deadline.rb
+++ lib/after_the_deadline.rb
@@ -116,19 +116,19 @@ private
 end
 
 class AfterTheDeadline::Metrics
-  attr_reader :spell, :grammer, :stats, :style
+  attr_reader :spell, :grammar, :stats, :style
 
   def initialize(array)
     unless array.kind_of?(Array) && array.all? {|i| i.kind_of?(Hash) }
       raise "#{self.class} must be initialized with an Array of Hashes"
     end
 
-    self.spell, self.grammer, self.stats, self.style = {}, {}, {}, {}
+    self.spell, self.grammar, self.stats, self.style = {}, {}, {}, {}
     array.each do |metric|
       self.send(metric['type'])[metric['key']] = metric['value']
     end
   end
 
 private
-  attr_writer :spell, :grammer, :stats, :style
+  attr_writer :spell, :grammar, :stats, :style
 end
